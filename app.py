5565d848607acf99839208236022e82bf65c4f7b
Uriel Fisher <urielfisher2004@gmail.com>
2025-02-22 11:33:48 +0200

    CORS support for dev stage

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import os
 import requests
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+cors = CORS(app, resources={'*': {'origins':"http://localhost:5173"}})
+
 @app.post('/ocr')
 def getOCR():
-  allowed = checkOCRUsage()
-  if allowed:
+  if not ocrAllowed():
     return 'no OCR', 503
   imageData = request.data
   if imageData == '':
@@ -15,13 +17,17 @@ def getOCR():
   return getOCRResponse(imageData)
 
 
-def checkOCRUsage():
+def ocrAllowed():
   return True
 
 
 def getOCRResponse(imageData):
   api_key = os.environ.get("GCP_API_KEY", False)
-  res = requests.post(f"https://vision.googleapis.com/v1/images:annotate?key={api_key}", data=imageData)
-  return res
+  res = requests.post(
+    url=f"https://vision.googleapis.com/v1/images:annotate?key={api_key}",
+    data=imageData,
+    headers={"Content-Type": "application/json"},
+  )
+  return res.text, res.status_code
 
 app.run(host='0.0.0.0', debug=True)

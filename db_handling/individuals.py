39aaa5a8ae6de235880ae1a05fbddb220c9d5fb3
Uriel Fisher <urielfisher2004@gmail.com>
2025-04-17 12:17:55 +0300

    CRUD on individuals (1/3), no user id yet, update DB initiation file

--- db_handling/individuals.py
+++ db_handling/individuals.py
@@ -5,13 +5,19 @@ app = Blueprint('individual', __name__)
 
 # user
 
+fields = [
+  'ownerId',
+  'name',
+  'pstn'
+]
+
 @app.get('/')
 def ind():
   with sqlite3.connect('db.db') as conn:
     cur = conn.cursor()
     res = cur.execute('''
                 SELECT * FROM individuals
-                WHERE owner = 1
+                WHERE ownerId = 1
                 ''').fetchall()
     return res
 
@@ -24,55 +30,77 @@ def addIndividual():
     cur = conn.cursor()
     dct = {'name': '', 'pstn': None}
     dct = {**dct, **request.json}
-    dct.update({'owner': '1'})
+    dct.update({'ownerId': '1'})
+
+    if not dct['name']:
+      return 'Insufficient individual details', 400
 
     if indivExists({
-      'owner': dct['owner'],
+      'ownerId': dct['ownerId'],
       'name': dct['name']
       }):
       return 'Individual already exists', 400
     
-    cur.execute('INSERT INTO individuals (owner, name) VALUES (:owner, :name)',
+    cur.execute('INSERT INTO individuals (ownerId, name) VALUES (:ownerId, :name)',
       dct)
     return 'ok'
   
 
-@app.patch('/')
-def updateIndividual():
-  if(len(request.args) == 0):
+@app.patch('/<string:name>')
+def updateIndividual(name):
+  ownerId = 1
+  changes = request.json
+  if not changes:
     return 'No changes supplied', 400
+  if not all(field in fields for field in changes):
+    return 'Inappropriate field names supplied for change', 400
+
   with sqlite3.connect('db.db') as conn:
     cur = conn.cursor()
 
     if not indivExists({
-      'owner': '1',
-      'name': request.args.get('name', '')
+      'ownerId': '1',
+      'name': name
       }):
       return 'Individual to change not found', 400
     
-    cur.execute('UPDATE individuals SET name = ? WHERE owner = ?',
-                (request.args.get('name', ''), 1))
-    return 'ok'
+    flds = []
+    values = []
+    for key, value in changes.items():
+      flds.append(f'{key} = ?')
+      values.append(value)
+    values.append(ownerId)
+    
+    cur.execute(f'UPDATE individuals SET {", ".join(flds)} WHERE ownerId = ?',
+                values)
+    return 'ok', 204
 
 
-@app.delete('/')
-def deleteIndividual():
-  if(not request.args.get('name', None)):
+@app.delete('/<string:name>')
+def deleteIndividual(name):
+  if not name:
     return 'No individual name supplied', 400
   with sqlite3.connect('db.db') as conn:
+    if not indivExists({
+      'ownerId': '1',
+      'name': name
+      }):
+      return 'Individual to delete not found', 404
     cur = conn.cursor()
-    cur.execute('DELETE FROM individuals WHERE owner = ? AND name = ?',
-                (1, request.args['name']))
-    return 'ok'
+    cur.execute('DELETE FROM individuals WHERE ownerId = ? AND name = ?',
+                (1, name))
+    return 'ok', 204
   
 
 def indivExists(attributes):
   with sqlite3.connect('db.db') as conn:
     cur = conn.cursor()
     query = 'SELECT * FROM individuals WHERE'
-    for att, value in attributes:
-      query += f' {att} = {value}'
-    if(len(cur.execute(query).fetchone()) == 0):
+    flds = []
+    for att, value in attributes.items():
+      flds.append(f' {att} = "{value}"')
+    print(query+' AND'.join(flds))
+    if(len(cur.execute(query+' AND'.join(flds)).fetchall()) == 0):
       return False
     return True
     
--- db_handling/initDB.py
+++ db_handling/initDB.py
@@ -4,11 +4,31 @@ conn = sqlite3.connect('db.db')
 
 cur = conn.cursor()
 
-# cur.execute('DROP TABLE IF EXISTS individuals')
+cur.execute('DROP TABLE IF EXISTS users')
+cur.execute('DROP TABLE IF EXISTS individuals')
+cur.execute('DROP TABLE IF EXISTS records')
+
+cur.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            settings TEXT NOT NULL
+)''')
 
 cur.execute('''
             CREATE TABLE IF NOT EXISTS individuals (
-            owner INTEGER NOT NULL,
-            name TEXT NOT NULL,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ownerId INTEGER NOT NULL,
+            name TEXT,
             pstn INTEGER
+)''')
+
+cur.execute('''
+            CREATE TABLE IF NOT EXISTS records (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            indivId INTEGER NOT NULL,
+            date INTEGER NOT NULL DEFAULT (strftime('%s', 'now')),
+            rates TEXT CHECK(json_valid(rates)),
+            sums TEXT CHECK(json_valid(rates)),
+            total NUMBER NOT NULL
 )''')
